12ff3eee7085f19a097d663597b76f9166d239b5
Anandya Almas <byanmalang123@gmail.com>
2024-11-08 06:55:38 +0700

    percobaan 1 nomor 6

--- Bioskop03.java
+++ Bioskop03.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 public class Bioskop03 {
 
-    public static void main(String[ ] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] [] penonton = new String[4][2];
-        
+        String[][] penonton = new String[4][2];
+
         penonton[0][0] = "Amin";
-        penonton[0] [1] = "Bena";
-        penonton [1] [0] = "Candra";
-        penonton[1 ][1] = "Dela";
-        penonton [2] [0] = "Eka";
-        penonton [2] [1 ] = "Farhan";
-        penonton [3][0] = "Gisel";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Della";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
-        System.out. printf( "%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3] [0], penonton[3][1]);
-
-}
+        for (String[] baris : penonton) {
+            System.out.printf("%s \t %s\n", baris[0], baris[1]);
+        }
+        int index = 0;
+        for (String[] baris : penonton) {
+            System.out.println("Panjang baris " + ": " + baris.length);           
+        
+        }
+        System.out.println("jumlah total baris: " + penonton.length);
+        
+    }
+    
 }
