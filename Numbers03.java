c9422cab632e387876599c744d8dcef0d67f87aa
Anandya Almas <byanmalang123@gmail.com>
2024-11-08 07:42:13 +0700

    percobaan 3 nomor 1

--- Numbers03.java
+++ Numbers03.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Numbers03 {
@@ -7,6 +8,11 @@ public class Numbers03 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
-    }
 
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+    }
+    
 }
+
