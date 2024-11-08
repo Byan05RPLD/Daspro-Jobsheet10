cc4a17325dd64db48e5423adfb4cea9f86809549
Anandya Almas <byanmalang123@gmail.com>
2024-11-08 07:45:56 +0700

    percobaan 3 nomor 4

--- Numbers03.java
+++ Numbers03.java
@@ -12,7 +12,11 @@ public class Numbers03 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
     }
-    
 }
+}
+
+
 
