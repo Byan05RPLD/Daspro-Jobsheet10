9e2936ef4472b79d620853860b83f0808876e6b0
Anandya Almas <byanmalang123@gmail.com>
2024-11-08 07:02:52 +0700

    percobaan 1 nomor 6

--- Bioskop03.java
+++ Bioskop03.java
@@ -5,28 +5,22 @@ public class Bioskop03 {
         Scanner sc = new Scanner(System.in);
         String[][] penonton = new String[4][2];
 
-        penonton[0][0] = "Amin";
-        penonton[0][1] = "Bena";
+        penonton[0][0] = "Amin" ;
+        penonton[0][1] = "Bena" ;
         penonton[1][0] = "Candra";
-        penonton[1][1] = "Della";
+        penonton[1][1] = "Dela";
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-        penonton[3][1] = "Hana";
+        penonton[3][1] = "Mina" ;
 
         for (String[] baris : penonton) {
             System.out.printf("%s \t %s\n", baris[0], baris[1]);
         }
-        int index = 0;
         for (String[] baris : penonton) {
-            System.out.println("Panjang baris " + ": " + baris.length);           
+        System.out.println("Panjang baris " + ": " + baris.length);
         }
-        System.out.println("Nama penonton pada baris ke-3:");
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][1]);
-        }
-        System.out.println("jumlah total baris: " + penonton.length);
-        
+
+        System.out.println("Jumlah total baris: " + penonton.length);
     }
-    
-}
+    }
