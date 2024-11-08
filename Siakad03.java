f2342e925117af87f85fb1d91db8f9b0f93afc84
Anandya Almas <byanmalang123@gmail.com>
2024-11-08 08:09:41 +0700

    pertanyaan 1 siakad

--- Siakad03.java
+++ Siakad03.java
@@ -3,31 +3,37 @@ import java.util.Scanner;
 public class Siakad03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
 
-        for (int i = 0; i < nilai.length; i++) {
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMataKuliah = sc.nextInt();
+
+        int[][] nilai = new int[jumlahMahasiswa][jumlahMataKuliah];
+
+        for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
-            for (int j = 0; j < nilai[i].length; j++) {
+            for (int j = 0; j < jumlahMataKuliah; j++) {
                 System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
-                totalPerSiswa += nilai[i][j]; 
-
+                totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPerSiswa / 3);
+            System.out.println("Nilai rata-rata mahasiswa ke-" + (i + 1) + ": " + totalPerSiswa / jumlahMataKuliah);
             System.out.println("\n================================");
-
         }
-        System.out.println("rata-rata nilai setiap mata kuliah:");
-        for (int j = 0; j < 3; j++) {
+
+        System.out.println("Rata-rata nilai setiap mata kuliah:");
+        for (int j = 0; j < jumlahMataKuliah; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahMahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
-
+            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / jumlahMahasiswa);
         }
+
+        sc.close();
     }
 }
