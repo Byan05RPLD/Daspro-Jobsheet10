a29e3a022399a922e0f4d6142def50cd67b5ac74
Anandya Almas <byanmalang123@gmail.com>
2024-11-08 07:57:44 +0700

    percobaan 4 nomor 6

--- Siakad03.java
+++ Siakad03.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class Siakad03 {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int [4][3];
+        int[][] nilai = new int[4][3]; 
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
 
             for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+i) + ": ");
+                
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
             }
         }
+
+       
+        sc.close();
     }
 }
-
-
